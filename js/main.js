6c0e1d019587d2eb972283846335e227d7442926
TaiserDeflos <57991395+TaiserDeflos@users.noreply.github.com>
2020-03-07 23:36:18 +0700

    h

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function navbtn(){
+let ul = document.getElementById('ul');
+ul.style.display = 'block';
+}
+function open1(){
+	let a = document.getElementById('1');
+	a.style.display = 'block';
+	let b = document.getElementById('2');
+	b.style.display = 'none';
+	let c = document.getElementById('3');
+	c.style.display = 'none';
+	let d = document.getElementById('4');
+	d.style.display = 'none';
+}
+function open2(){
+	let a = document.getElementById('1');
+	a.style.display = 'none';
+	let b = document.getElementById('2');
+	b.style.display = 'block';
+	let c = document.getElementById('3');
+	c.style.display = 'none';
+	let d = document.getElementById('4');
+	d.style.display = 'none';
+}
+function open3(){
+	let a = document.getElementById('1');
+	a.style.display = 'none';
+	let b = document.getElementById('2');
+	b.style.display = 'none';
+	let c = document.getElementById('3');
+	c.style.display = 'block';
+	let d = document.getElementById('4');
+	d.style.display = 'none';
+}
+function open4(){
+	let a = document.getElementById('1');
+	a.style.display = 'none';
+	let b = document.getElementById('2');
+	b.style.display = 'none';
+	let c = document.getElementById('3');
+	c.style.display = 'none';
+	let d = document.getElementById('4');
+	d.style.display = 'block';
+}
