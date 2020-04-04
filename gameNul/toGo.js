04aaeb9ae97b2f673c65b37125e2dd3c415d96e4
TaiserDeflos <57991395+TaiserDeflos@users.noreply.github.com>
2020-04-04 19:13:46 +0800

    game1

---
+++ gameNul/toGo.js
@@ -0,0 +1,23 @@
+function bt(){
+let playerFirst = document.getElementById('inputFirst').value;
+console.log(playerFirst);
+}
+bt();
+function btnTwo(){
+let playerTwo = document.getElementById('inputTwo').value;
+let playerFirst = document.getElementById('inputFirst').value;
+console.log(playerTwo);
+console.log(playerFirst);
+if(playerTwo == playerFirst){
+	alert("Вы угадали!");
+}
+else if(playerTwo > playerFirst){
+	alert("Ты ввёл больше, чем нужно!")
+}
+else if(playerTwo < playerFirst){
+	alert("Ты ввёл меньше, чем нужно!")
+}
+else{
+	alert("Ошибка!");
+}
+}
